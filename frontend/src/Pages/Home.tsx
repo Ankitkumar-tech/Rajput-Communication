36a95437b03d113b11a6f814b36a6aeb31dc7fc4
Ankitkumar-tech <ankit8287670719@gmail.com>
2023-11-07 22:46:44 +0530

    Create navigation bar and integrate with login and signup

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,6 +1,9 @@
+import Navbar from "./Navbar"
+
 export const Home =()=>{
     return(
         <>
+        <Navbar/>
        <h1> Welcome to Homepage</h1>
         </>
     )
---
+++ frontend/src/Pages/Navbar.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./Navbar.css"
+import { Link } from 'react-router-dom'
+const Navbar = () => {
+
+    
+  return (
+    <div  className='Nav-mainDiv'>
+     <img
+            className="Nav_logo_img"
+            src="https://i.ibb.co/NW02kFD/Smart-Online-Shop-Chart-Logo.png"
+            alt="ShopLogo"
+          />
+        <ul>
+            <li><Link style={{textDecoration:"none",color:"black"} } to={'/'}> Product </Link></li>
+            {/* <li>About</li> */}
+            <li><Link style={{textDecoration:"none",color:"black"}} to={''}> Contact </Link></li>
+            <li><Link style={{textDecoration:"none",color:"black"}} to={'/register'}> Sign up</Link></li>
+            <li><Link style={{textDecoration:"none",color:"black"}} to={'/logn'}> Sign in</Link></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
