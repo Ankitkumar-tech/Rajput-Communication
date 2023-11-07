500570cd579737f6799fd8c678897cb3f5db449b
Ankitkumar-tech <ankit8287670719@gmail.com>
2023-11-07 23:58:55 +0530

    addeed image scroller

---
+++ frontend/src/Component/Context/auth.tsx
@@ -0,0 +1,18 @@
+// import { createContext } from "react";
+
+
+// const AuthContext  =createContext();
+
+
+// const AuthProvider  =()=>{
+// const [auth,setAuth] =useState({
+//     user:null,
+//     token:""
+// })
+
+// return(
+//     <AuthContext.Provider>
+    
+//     </AuthContext.Provider>
+// )
+// }
